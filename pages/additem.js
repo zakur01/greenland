d22d3a2a938644d9fdc7577dda82e93d64946240
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2022-05-22 17:18:32 +0600

    formdata import

--- pages/additem.js
+++ pages/additem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import FormData from 'form-data';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
