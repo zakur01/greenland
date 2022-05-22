f5b90ad3bdd010e3c0cddfe81b92eb40108d2708
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2022-05-22 17:23:09 +0600

    admin

--- pages/admin.js
+++ pages/admin.js
@@ -5,13 +5,13 @@ function Admin({ user }) {
 }
 
 // взять информацию о юзере
-export async function getStaticProps() {
-  const res = await fetch('https://greenlandstrapi.herokuapp.com/api/me');
-  const data = res.json();
-  const user = data.data;
-  return {
-    props: { user },
-  };
-}
+// export async function getStaticProps() {
+//   const res = await fetch('https://greenlandstrapi.herokuapp.com/api/me');
+//   const data = res.json();
+//   const user = data.data;
+//   return {
+//     props: { user },
+//   };
+// }
 
-export default Admin;
+// export default Admin;
