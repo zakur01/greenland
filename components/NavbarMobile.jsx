c05a73c4ad7cf43124d72859928fcd30d9add7c0
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2022-05-23 14:51:13 +0600

    css for mobile navbar

--- components/NavbarMobile.jsx
+++ components/NavbarMobile.jsx
@@ -16,10 +16,13 @@ function NavbarMobile() {
   const logginout = () => {
     dispatch(reset());
     dispatch(LogOut());
+    setNavVisibility(!navVisibility);
     router.push('/');
   };
-  const filter = (e) => {
+  const filterMobile = (e) => {
     router.push('/');
+    setNavVisibility(!navVisibility);
+
     setTimeout(() => {
       const filt = e.target.id;
       dispatch(FilterItems(filt));
@@ -49,19 +52,19 @@ function NavbarMobile() {
       <div className={navVisibility ? styles.nav2 : styles.nav}>
         <div>
           <Link href="/">
-            <h1>GreenLand</h1>
+            <h1 onClick={() => setNavVisibility(!navVisibility)}>Главная</h1>
           </Link>
         </div>
         <div className={styles.nav_list}>
           <Link href="/aboutus">
-            <h1>О нас</h1>
+            <h1 onClick={() => setNavVisibility(!navVisibility)}>О нас</h1>
           </Link>
           <Link href="/bin">
-            <h1>Корзина</h1>
+            <h1 onClick={() => setNavVisibility(!navVisibility)}>Корзина</h1>
           </Link>
           {isAuthenticated && (
             <Link href="/saved">
-              <h1>
+              <h1 onClick={() => setNavVisibility(!navVisibility)}>
                 Сохранённые
                 {/* {saved_lil.data.length > 0 ? saved_lil.data.length : ''}) */}
               </h1>
@@ -70,22 +73,28 @@ function NavbarMobile() {
           {isAuthenticated ? (
             <div>
               <Link href="/profile">
-                <h1>Профиль ({User})</h1>
+                <h1 onClick={() => setNavVisibility(!navVisibility)}>
+                  Профиль ({User})
+                </h1>
               </Link>
             </div>
           ) : (
             <Link href="/register">
-              <h1>Регистрация</h1>
+              <h1 onClick={() => setNavVisibility(!navVisibility)}>
+                Регистрация
+              </h1>
             </Link>
           )}
           {User == 'admin' && (
             <Link href="/additem">
-              <h1>Добавить товар</h1>
+              <h1 onClick={() => setNavVisibility(!navVisibility)}>
+                Добавить товар
+              </h1>
             </Link>
           )}
           {isAuthenticated ? null : (
             <Link href="/login">
-              <h1>Логин</h1>
+              <h1 onClick={() => setNavVisibility(!navVisibility)}>Логин</h1>
             </Link>
           )}
           {isAuthenticated && (
@@ -97,23 +106,30 @@ function NavbarMobile() {
         <div className={styles.leftmenu}>
           <ul>
             <Link href="/">
-              <li id="cosmetics" onClick={filter}>
+              <li id="cosmetics" onClick={filterMobile}>
                 Косметика
               </li>
             </Link>
-            <li id="supplements" onClick={filter}>
+            <li id="supplements" onClick={filterMobile}>
               БАДы
             </li>
-            <li id="home" onClick={filter}>
+            <li id="home" onClick={filterMobile}>
               Для дома
             </li>
-            <li id="books" onClick={filter}>
+            <li id="books" onClick={filterMobile}>
               Книги
             </li>
-            <li id="other" onClick={filter}>
+            <li id="other" onClick={filterMobile}>
               Разное
             </li>
-            <li onClick={() => dispatch(GetItems())}>Всё</li>
+            <li
+              onClick={() => {
+                dispatch(GetItems());
+                setNavVisibility(!navVisibility);
+              }}
+            >
+              Всё
+            </li>
           </ul>
         </div>
       </div>
